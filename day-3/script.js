03396ace27b24335e6ac0f80e0027fae6ccb9475
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-05-14 17:47:25 +0100

    Refactor the code to use javascript substring method to generate the hex code color

--- day-3/script.js
+++ day-3/script.js
@@ -1,12 +1,18 @@
 const hexContainer = document.querySelector('.hex-code');
 const generate = document.querySelector('.generate');
 
-const generateRandomHexColor = () => {
-  const randomHexColor = Math.floor(Math.random() * 10000000).toString(16)
-  document.body.style.backgroundColor = `#${randomHexColor}`;
-  hexContainer.innerHTML = `#${randomHexColor}`;
+// const generateRandomHexColor = () => {
+//   const randomHexColor = Math.floor(Math.random() * 16777215).toString(16)
+//   document.body.style.backgroundColor = `#${randomHexColor}`;
+//   hexContainer.innerHTML = `#${randomHexColor}`;
+// }
+
+const randomHexColor = () => {
+  const randomHexColorNumber = Math.random().toString(16).substring(2, 8);
+  document.body.style.backgroundColor = `#${randomHexColorNumber}`;
+  hexContainer.innerHTML = `#${randomHexColorNumber}`;
 }
 
-generate.addEventListener('click', generateRandomHexColor);
+generate.addEventListener('click', randomHexColor);
 
-generateRandomHexColor();
+randomHexColor();
