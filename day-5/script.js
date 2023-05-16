19aa0b30d30855aa2dc312f18dc37ad0c88f872b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-05-16 11:58:38 +0100

    Re-practice popup modal with vanilla js

---
+++ day-5/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.popup'),
+      close = document.querySelector('.close'),
+      modal = document.querySelector('.modal');
+
+btn.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+// modal.addEventListener('click', closeModal);
+
+function openModal(e) {
+  e.preventDefault();
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
