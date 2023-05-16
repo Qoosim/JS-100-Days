25366ba3f02cdfa396ff05b417ff24bd9d27b35a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-05-16 15:29:49 +0100

    Build Palindrome checker app

--- day-5/script.js
+++ day-5/script.js
@@ -1,16 +1,36 @@
-const btn = document.querySelector('.popup'),
-      close = document.querySelector('.close'),
-      modal = document.querySelector('.modal');
+const btn = document.querySelector('.check-btn');
+const result = document.querySelector('.result');
 
-btn.addEventListener('click', openModal);
-close.addEventListener('click', closeModal);
-// modal.addEventListener('click', closeModal);
+function palindrome() {
+  const word = document.querySelector('.input-text').value;
+  if (!word) return;
 
-function openModal(e) {
-  e.preventDefault();
-  modal.style.display = 'block';
+  let len = word.length;
+  let start = word.substring(0, Math.floor(len / 2));
+  let end = word.substring(len - Math.floor(len / 2));
+
+  // let flip = end.split("").reverse().join("");
+
+  let flip = [...end].reverse().join("");
+  if (start === flip) {
+    result.innerHTML = `${word} is a palindrome`;
+    timeOut();
+  } else {
+    result.innerHTML = `${word} is NOT a palindrome`;
+    timeOut();
+  }
+
 }
 
-function closeModal() {
-  modal.style.display = 'none';
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  palindrome()
+  document.querySelector('.input-text').value = '';
+  document.querySelector('.input-text').focus();
+});
+
+function timeOut() {
+  setTimeout(() => {
+    result.innerHTML = '';
+  }, 3000)
 }
